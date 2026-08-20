ae58e403ab4adb42fd0707c97055184386ac60bf
mj-aballe <mj.aballe@smsupermalls.com>
2026-08-20 10:47:40 +0800

    adjust the layout

--- src/App.jsx
+++ src/App.jsx
@@ -239,7 +239,7 @@ export default function App() {
 
             <div className="w-full flex items-center justify-center py-5 bg-white">
               <h2
-                className="font-brand font-light tracking-tight leading-none  text-[#2C1E1B] block uppercase text-3xl sm:text-5xl text-center select-none"
+                className="font-brand font-light text-[2.2rem] sm:text-[3.5rem] tracking-[0.25em] leading-none  text-[#2C1E1B] block uppercase text-center select-none"
               >
                 {heroConfig.aboutTitle || 'About Aura'}
               </h2>
