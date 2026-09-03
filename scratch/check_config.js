71bba9cba0596c53acaafb3714496b874e797a55
Mark Joseph Aballe <markmjaballe@gmail.com>
2026-09-03 09:03:05 +0800

    created new project on supabase

--- scratch/check_config.js
+++ scratch/check_config.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://sylfhockkibohntgcswb.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5bGZob2Nra2lib2hudGdjc3diIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxMzA3MjIsImV4cCI6MjEwMDcwNjcyMn0.audxFrkyQoBMhkN72WGF0LP_ip7eYnpCHVwkY3RXfVs';
+const supabaseUrl = 'https://pcmlleeuxbymomjxhwlv.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjbWxsZWV1eGJ5bW9tanhod2x2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgzNzA2NzgsImV4cCI6MjEwMzk0NjY3OH0.WZCqgnt_7l88U-Yl_rAvhmXQLSKy70hT1Wjq8rf_qC0';
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
